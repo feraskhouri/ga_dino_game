48997403e73e4db697af44769bd0afe7b9260af3
feras <khouryferas@hotmail.com>
2023-12-24 21:11:06 +0300

    testing

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 import numpy as np
 # from genetic import Genetic
 pygame.init()
-
+################################################################
 
 # Global Constants
 SCREEN_HEIGHT = 600
